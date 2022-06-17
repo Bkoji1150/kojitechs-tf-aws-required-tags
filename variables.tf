9088374a9cc6e9d9d0622b798f3bf4e79356f4d5
kojitechs <85324828+Bkoji1150@users.noreply.github.com>
2022-06-17 00:48:56 -0700

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -35,7 +35,7 @@ variable "operational_environment" {
   type        = string
 
   validation {
-    condition     = contains(["SBX", "DEV", "TEST", "IMPL", "TFI", "PROD"], var.operational_environment)
+    condition     = contains(["SBX", "DEV", "SHARED"], var.operational_environment)
     error_message = "The operational_environment value must be one of \"SBX\", \"DEV\", \"TEST\", \"IMPL\", \"TFI\", \"PROD\"."
   }
 }
